f63e95a55c6672a27e2d439161bdde39891dd317
Mohammad Roohitavaf <roohitavaf@gmail.com>
2024-11-02 18:25:20 -0700

    added lock

--- src/single_in_memory/kv_server_impl.hxx
+++ src/single_in_memory/kv_server_impl.hxx
@@ -2,15 +2,18 @@
 
 #include <string>
 #include <map>
+#include <mutex>
 
 namespace nuraft_on_the_rocks {
 
 class SingleInMemory final : public KeyValueServer {
 private:
     std::map<std::string, std::string> map_;
+    std::mutex mtx_;
 
 public:
     int getKey(std::string key, std::string& value) {
+        std::unique_lock<std::mutex> lk(mtx_);
         auto it = map_.find(key);
         if (it != map_.end()) {
             value = it->second;
@@ -20,15 +23,18 @@ public:
         }
     }
     int putKey(std::string key, std::string value) {
+        std::unique_lock<std::mutex> lk(mtx_);
         map_[key] = value;
         return 0;
     }
     int deleteKey(std::string key) {
+        std::unique_lock<std::mutex> lk (mtx_);
         map_.erase(key);
         return 0;
 
     }
     int scanKey(std::string startKey, std::string endKey, std::vector<std::pair<std::string, std::string>>& keyValues) {
+        std::unique_lock<std::mutex> lk(mtx_);
         auto it = map_.lower_bound(startKey);
         while (it != map_.end() && it->first <= endKey) {
             keyValues.emplace_back(it->first, it->second);
